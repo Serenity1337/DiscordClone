25569542c1e8adcc03498b35898abbf65dead07e
serenity <43002637+Serenity1337@users.noreply.github.com>
2021-03-10 19:17:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import { Switch, Route } from 'react-router-dom'
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </div>
+  )
+}
+
+export default App
